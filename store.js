d09c5193ca22423b70cb6e13458b382497efecc2
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-02-16 11:33:38 +0100

    dabatase mock definition

---
+++ store.js
@@ -0,0 +1,20 @@
+class Store {
+
+    state = [];
+
+    constructor() {
+        console.log("se ha creado una instancia de Store")
+
+
+    };
+
+    async set(object) {
+        this.state.push(object)
+        return this.state.length;
+    }
+    async get() {
+        return this.state.slice(-10);
+    }
+}
+
+module.exports = Store;
